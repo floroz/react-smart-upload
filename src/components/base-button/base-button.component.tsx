476fff8fe71598232c921e89bd2df14e1bc059d6
Daniele Tortora <daniele.tortora@frontiersin.org>
2024-02-06 12:08:39 +0100

    feat: extend button to support polymorphism

--- src/components/base-button/base-button.component.tsx
+++ src/components/base-button/base-button.component.tsx
@@ -3,41 +3,64 @@ import styles from "./base-button.module.css";
 
 type BaseButtonVariant = "primary" | "secondary" | "tertiary";
 
-type BaseButtonProps = React.DetailedHTMLProps<
-  React.ButtonHTMLAttributes<HTMLButtonElement>,
-  HTMLButtonElement
-> & {
+type BaseButtonProps = {
+  /**
+   * The label of the button, to be added for accessibility when used as Icon Button.
+   */
+  label?: string;
   children: React.ReactNode;
   /**
    * The variant of the button
    */
   variant: BaseButtonVariant;
   disabled?: boolean;
+  /**
+   * Polymorphic prop to allow the button to be rendered as an anchor or a button
+   */
+  as?: React.ElementType;
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
+  /**
+   * If true, the button will take the full width of its parent
+   * @default false
+   */
+  fullWidth?: boolean;
+} & {
+  [domAttribute: string]: unknown;
 };
 
 /**
- * This should use a `forwardRef` but due to the Storybook bug I want to keep a working example of the documentation.
+ * This should use a `forwardRef` but due to the Storybook bug I want to keep a working example of the documentation Controls.
  */
 const BaseButton = ({
   children,
   variant = "primary",
   disabled,
-  ...props
+  fullWidth,
+  leftIcon,
+  rightIcon,
+  label,
+  as: Component = "button",
+  ...domAttributes
 }: BaseButtonProps) => {
   return (
-    <button
-      type="button"
+    <Component
+      {...(Component === "button" ? { type: "button" } : {})}
       disabled={disabled}
-      {...props}
+      {...domAttributes}
       className={clsx([styles.base, "with-focus"], {
         [styles.primary]: variant === "primary",
         [styles.secondary]: variant === "secondary",
         [styles.tertiary]: variant === "tertiary",
+        [styles.fullWidth]: fullWidth,
         [styles.disabled]: disabled,
       })}
+      aria-label={label}
     >
+      {leftIcon && <span className={styles.leftIcon}>{leftIcon}</span>}
       {children}
-    </button>
+      {rightIcon && <span className={styles.rightIcon}>{rightIcon}</span>}
+    </Component>
   );
 };
 
--- src/components/base-button/base-button.stories.tsx
+++ src/components/base-button/base-button.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from "@storybook/react";
 import BaseButton from "./base-button.component";
+import { MdShoppingCart } from "react-icons/md";
 
 const meta: Meta<typeof BaseButton> = {
   title: "Components/BaseButton",
@@ -29,5 +30,62 @@ export const Tertiary: Story = {
   args: {
     variant: "tertiary",
     children: "Click Me",
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    variant: "primary",
+    children: "Click Me",
+    disabled: true,
+  },
+};
+
+export const FullWidth: Story = {
+  args: {
+    variant: "primary",
+    children: "Click Me",
+    fullWidth: true,
+  },
+  decorators: [
+    (Story) => (
+      <div style={{ width: "500px" }}>
+        Wrapping Container: 500px
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export const WithLeftIcon: Story = {
+  args: {
+    variant: "primary",
+    children: "Click Me",
+    leftIcon: <MdShoppingCart />,
+  },
+};
+
+export const WithRightIcon: Story = {
+  args: {
+    variant: "primary",
+    children: "Click Me",
+    rightIcon: <MdShoppingCart />,
+  },
+};
+
+export const AsAnchor: Story = {
+  args: {
+    variant: "primary",
+    children: "Click Me",
+    as: "a",
+    href: "#",
+  },
+};
+
+export const IconButton: Story = {
+  args: {
+    variant: "primary",
+    label: "Shopping Cart",
+    children: <MdShoppingCart />,
   },
 };
